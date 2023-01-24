8ddb090ca766c8ca5f40c257cd6ba5aaf61cc70c
lordSosa15 <97013669+lordSosa15@users.noreply.github.com>
2023-01-23 20:11:19 -0500

    improved solution

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -2,20 +2,24 @@ package tech.strategio;
 
 import java.util.Scanner;
 
-public class LoanCalculator {
 
+public class LoanCalculator {
     /**
      * Calculates the remaining loan amount after 3 months.
      * @param amount The initial loan amount
      * @return The remaining loan amount after 3 months.
      */
+    // each month remove 10 percent of the total
+    // for 3 months
+    // to get the 10 percent divide the number divided by 10 / then subtract that from the number
+    // loop 3 times for each month
+    // starts at 0 then 1 then 2 never reaches 3 (we are trying to get 3 months so that's 012 = 3
     static int getRemainingAmountIn3Months(int amount) {
-        int remainingAmount = amount;
+
         for(int i = 0; i < 3; i++){
-            int monthlyPayment = (int)(remainingAmount * 0.1);
-            remainingAmount -= monthlyPayment;
+            amount = amount - (amount  /10);
         }
-        return remainingAmount;
+        return amount;
     }
 
     public static void main(String[] args) {
